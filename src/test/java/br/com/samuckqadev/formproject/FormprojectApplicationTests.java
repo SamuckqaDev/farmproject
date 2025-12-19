bc7f75e9b579bebf1804345213dada26588fa7c9
samuckqadev <samuckqa@gmail.com>
2025-12-18 22:04:13 -0300

    initial commit

---
+++ src/test/java/br/com/samuckqadev/formproject/FormprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.samuckqadev.formproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
