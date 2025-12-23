112e71700b717b19383a2b943b852b17fcd33110
samuckqadev <samuckqa@gmail.com>
2025-12-23 02:09:58 -0300

    fix: tests

--- src/test/java/br/com/samuckqadev/formproject/FormprojectApplicationTests.java
+++ src/test/java/br/com/samuckqadev/formproject/FormprojectApplicationTests.java
@@ -3,11 +3,14 @@ package br.com.samuckqadev.formproject;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// Adicionamos a referência explícita à sua classe principal
+@SpringBootTest(classes = FormprojectApplicationTests.class)
 class FormprojectApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        // Se este teste passar, significa que seu banco, liquibase 
+        // e todas as dependências estão configuradas corretamente.
+    }
 
 }
